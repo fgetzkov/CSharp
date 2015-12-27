dfbbf98a39bcc831cc5f2342cd4fd0d0d70d0bf2
Philip Getzkov <fgetzkov@gmail.com>
2015-12-27 14:57:05 +0200

    get bigger number

---
+++ BiggerNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class BiggerNumber
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        Console.WriteLine(getMax(x, y));
+    }
+
+    private static int getMax(int x, int y)
+    {
+        if (x>=y)
+        {
+            return x;
+        }
+        else
+        {
+            return y;
+        }
+    }
+}
