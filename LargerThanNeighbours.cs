a4eb1f5461070c8b53d4602dbe9d3fde9a3b2a28
Philip Getzkov <fgetzkov@gmail.com>
2015-12-27 15:54:18 +0200

    First Larger Number

---
+++ LargerThanNeighbours.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class LargerThanNeighbours
+{
+    static void Main()
+    {
+        int[] arr = {1,3,4,5,1,0,5};
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(IsLargerThanNeighbours(arr,i));
+        }
+    }
+
+    private static string IsLargerThanNeighbours(int[] arr, int i)
+    {
+        if (i==0)
+        {
+            if (arr[i]>arr[i+1])
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }
+        }
+        if (i==arr.Length-1)
+        {
+            if (arr[i] > arr[i - 1])
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }   
+        }
+        if (arr[i]>arr[i+1] && arr[i]<arr[i-1])
+        {
+            return "true";
+        }
+        else
+        {
+            return "false";
+        }
+    }
+}
+
