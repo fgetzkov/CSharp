a61bc41f34a9baf62d034518355a29960fa599dd
Philip Getzkov <fgetzkov@gmail.com>
2015-12-27 14:58:13 +0200

    get last digit of number

---
+++ LastDigitNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class LastDigitNumber
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(GetLastDigit(number));
+    }
+
+    private static int GetLastDigit(int number)
+    {
+        return number%10;
+    }
+}
+
